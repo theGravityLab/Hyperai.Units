5599e730247819d7ef3c9c163c7b5eacec8a7856
Chien Zhang <2419328026@qq.com>
2021-06-12 17:05:51 +0800

    - 小修改

--- Hyperai.Units/UnitMiddleware.cs
+++ Hyperai.Units/UnitMiddleware.cs
@@ -71,7 +71,7 @@ namespace Hyperai.Units
             _service.Handle(context);
             stopwatch.Stop();
             _logger.LogDebug("Handling for Unit Actions took {} milliseconds(preparing = {}): {}",
-                stopwatch.ElapsedMilliseconds + prepare, prepare, context.Message);
+                stopwatch.ElapsedMilliseconds + prepare, prepare, context.Message.ToString());
             stopwatch.Reset();
             return true;
         }
