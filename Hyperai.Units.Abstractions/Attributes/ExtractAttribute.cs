4ed5ea404c9bbd7f7c9973d4ffc377b3b83c93a0
Chien Zhang <zqy0224@live.com>
2021-07-31 19:13:06 +0800

    Extract中的变量允许存在大写字母

--- Hyperai.Units.Abstractions/Attributes/ExtractAttribute.cs
+++ Hyperai.Units.Abstractions/Attributes/ExtractAttribute.cs
@@ -17,10 +17,10 @@ namespace Hyperai.Units.Attributes
         {
             TrimSpaces = trimSpaces;
             RawString = pattern;
-            var parameters = Regex.Matches(pattern, @"\{(?<name>[a-z0-9]+)\}");
+            var parameters = Regex.Matches(pattern, @"\{(?<name>[A-Za-z0-9]+)\}");
             Names = parameters.Select(x => x.Groups["name"].Value).ToList();
             pattern = '^' + Regex.Escape(pattern).Replace(@"\*", ".*").Replace(@"\{", "{") + '$';
-            pattern = Regex.Replace(pattern, @"\{([a-z0-9]+)\}", @"([\S]+)");
+            pattern = Regex.Replace(pattern, @"\{([A-Za-z0-9]+)\}", @"([\S]+)");
             Pattern = new Regex(pattern);
         }
 
